9aaa46e5acee782dd4a6798d98b840841a74e939
rosangelaoliveira-prof <rosangelaoliveira03@prof.educacao.sp.gov.brgit config --listgit config --global user.email rosangelaoliveira03@prof.educacao.sp.gov.br>
2026-09-22 15:30:25 -0300

    Feature: Multiplicação/divisão e layout

--- script.js
+++ script.js
@@ -14,4 +14,28 @@ function subtrair() {
     let resultado = numero1 - numero2;
 
     document.getElementById("resultado").innerHTML = "Resultado: " + resultado;
+}function multiplicar() {
+    let numero1 = Number(document.getElementById("numero1").value);
+    let numero2 = Number(document.getElementById("numero2").value);
+
+
+    document.getElementById("resultado").innerText =
+        "Resultado: " + (numero1 * numero2);
+}
+
+
+function dividir() {
+    let numero1 = Number(document.getElementById("numero1").value);
+    let numero2 = Number(document.getElementById("numero2").value);
+
+
+    if (numero2 === 0) {
+        document.getElementById("resultado").innerText =
+            "Resultado: Não é possível dividir por zero.";
+        return;
+    }
+
+
+    document.getElementById("resultado").innerText =
+        "Resultado: " + (numero1 / numero2);
 }
